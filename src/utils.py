2af078a19462060fdfb0a32db269c6dd943c4c63
Siddhant Uke <ukesidd@gmail.com>
2024-09-08 11:20:11 +0530

    MODEL TRAINER

--- src/utils.py
+++ src/utils.py
@@ -5,7 +5,7 @@ import numpy as np
 import pandas as pd
 import dill
 import pickle
-from sklearn.metrics import r2_score
+from sklearn.metrics import r2_score ## WRITTEN L6 
 from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
@@ -22,7 +22,7 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
     
-def evaluate_models(X_train, y_train,X_test,y_test,models,param):
+def evaluate_models(X_train, y_train,X_test,y_test,models,param):  ### WRITTEN L6
     try:
         report = {}
 
